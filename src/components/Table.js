b2f5aa5c990e62866aad145c5c73f07cfade8875
AaronLaz <aaron.lazaroo@etu.umontpellier.fr>
2021-11-17 15:31:44 +0100

    add table to dashboard

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,15 +5,15 @@ const columns = [
     { field: 'id', headerName: 'CODE', width: 70 },
     { field: 'libelle', headerName: 'LIBELLE', width: 150 },
     { field: 'unite', headerName: 'UNITE', width: 70 },
-    { field: 'prixUnitaire', headerName: 'PRIXUNITAIRE', width: 90 },
-    { field: 'stocks', headerName: 'STOCKS', width: 60 },
-    { field: 'stocksVal', headerName: 'STOCKS VAL', width: 60 },
-    { field: 'allergène', headerName: 'ALLERGENE', width: 60 },
+    { field: 'prixUnitaire', headerName: 'PRIXUNITAIRE', width: 140 },
+    { field: 'stocks', headerName: 'STOCKS', width: 100 },
+    { field: 'stocksVal', headerName: 'STOCKS VAL', width: 120 },
+    { field: 'allergène', headerName: 'ALLERGENE', width: 130 },
   ];
 
   const rows = [
-    { id: "100", libelle: "Epaule d\'agneau sans os", unite: "Kg", prixUnitaire:"8,49 €",stocks: "", stocksVal:"" },
-    { id: "101", libelle: 'Filet de poulet', unite: "Kg", prixUnitaire:"4,12 €",stocks: "", stocksVal:"" },
+    { id: "100", libelle: "Epaule d\'agneau sans os", unite: "Kg", prixUnitaire:"8,49 €",stocks: "", stocksVal:"",allergène:true },
+    { id: "101", libelle: 'Filet de poulet', unite: "Kg", prixUnitaire:"4,12 €",stocks: "", stocksVal:"",allergène:false },
   ];
 
 export default function Table() {
