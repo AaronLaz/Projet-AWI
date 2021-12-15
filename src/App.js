8c1dca2e98663c849cf6e4bc80d22d8dd6e1088c
LiamRICE <71870225+LiamRICE@users.noreply.github.com>
2021-12-15 18:00:05 +0100

    routing is set up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Redirect, Route, Routes } from 'react-router-dom';
+
 import Header from './components/Header';
 import Menu from './components/Menu';
 import Dashboard from './components/Dashboard';
 import Footer from './components/Footer';
+import Table from './components/Table';
+
+
 
 function App() {
   return (
     <div className="wrapper">
-        <Header/>
-        <Menu />
-        <Dashboard />
-        <Footer />
+      <Router>
+        <Route exact path={['/', '/mercurial']}>
+          <div>
+            <Header/>
+          </div>
+          <div>
+            <Menu/>
+          </div>
+          <div>
+            <Route exact path="/" component={Dashboard}/>
+            <Route exact path="/mercurial" component={Table}/>
+          </div>
+          <div>
+            <Footer/>
+          </div>
+        </Route>
+      </Router>
     </div>
   );
 }
